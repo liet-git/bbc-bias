c42a663e7b66c9fa55018a99ab9888bc89488702
lietava-git <lietava.jan@gmail.com>
2023-11-17 12:23:59 +0000

    cleaning up base scraping script

--- general/scraping.py
+++ general/scraping.py
@@ -36,17 +36,22 @@ def load_full_article_data(directory: str):
     df = df.apply(get_article_info, axis=1)
 
     # articles can be in multiple topics
-    df.drop_duplicates('href', keep='first', inplac=True)
+    df = df.groupby(['title', 'href'])['topic'].apply(lambda x: ','.join(x)).reset_index()
 
     return df
 
 
 if __name__ == '__main__':
+
+    # load all BBC topics we want to scrape from a json file
     all_topics_json = load_json('../data/topics.json')
     base_url_topic = 'https://www.bbc.co.uk/news/topics'
 
+    # collect all base article data
     scrape_topics(all_topics_json, base_url_topic)
 
-    #summary_df = load_full_article_data('./data/metadata/')
+    # this can then be run to collect full info (i.e. date + full text + inner article title which can be different)
+    summary_df = load_full_article_data('../data/metadata/')
 
-    #summary_df.to_csv('./data/summary.csv')
+    # save scraped output to summary file
+    summary_df.to_csv('../data/summary_20231114.csv')
