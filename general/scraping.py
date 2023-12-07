6884d6f5a461833c1cdb13a3443bdee0e0f78c17
liet-git <lietava.jan@gmail.com>
2023-12-07 22:05:20 +0000

    returning base scraper to run for all topics

--- general/scraping.py
+++ general/scraping.py
@@ -83,13 +83,11 @@ if __name__ == '__main__':
         latest_summary = pd.read_csv(latest_summary_path[0])
 
     # load all BBC topics we want to scrape from a json file
-    #all_topics_json = load_json('../data/topics.json')
-    #base_url_topic = 'https://www.bbc.co.uk/news/topics'
-
-    #topics_now = {"Israel-Gaza war": "c2vdnvdg6xxt"}
+    all_topics_json = load_json('../data/topics.json')
+    base_url_topic = 'https://www.bbc.co.uk/news/topics'
 
     # collect all base article data, ignoring existing scraped articles
-    #scrape_topics(topics_now, base_url_topic, ignore=False)
+    scrape_topics(all_topics_json, base_url_topic, ignore=False)
 
     # this can then be run to collect full info (i.e. date + full text + inner article title which can be different)
     summary_df = load_full_article_data('../data/metadata/', existing_summary=latest_summary)
